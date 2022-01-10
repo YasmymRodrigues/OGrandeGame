61fdbbb618d063c43201bea77d797a3b47576556
learngit <yasmymrdgs@gmail.com>
2022-01-10 17:00:40 +0000

    Edited fun router()

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -6,7 +6,7 @@ enum class CommandType{GET, POST}
 
 fun getPlayer(manager: GameManager, args: List<String>): String? {return ""}
 
-fun router() : (GameManager, List<String>) -> String? { CommandType.GET ; return ::getPlayer}
+fun router(): (GameManager, List<String>) -> String? { CommandType.GET ; return ::getPlayer}
 //val f1 = router()
 //val f2 = f1.invoke(GameManager)
 //val result = f2.invoke(manager, listOf("PLAYER", "Joshua"))
