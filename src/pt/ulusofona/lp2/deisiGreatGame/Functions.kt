dcdee25e923561da56153915aaeea075d99b7f4f
learngit <yasmymrdgs@gmail.com>
2022-01-10 12:20:27 +0000

    Edited fun router()

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -1,11 +1,13 @@
 package pt.ulusofona.lp2.deisiGreatGame
 
+import kotlin.reflect.KFunction2
+
 
 enum class CommandType{GET, POST}
 
 fun getPlayer(manager: GameManager, args: List<String>): String?{return ""}
 
-fun router() : (GameManager, List<String>) -> String? { CommandType.GET ; return ::getPlayer }
+fun router() : KFunction2<GameManager, List<String>, String?> { CommandType.GET ; return ::getPlayer }
 
 //val f1 = router()
 //val f2 = f1.invoke(CommandType.GET)
