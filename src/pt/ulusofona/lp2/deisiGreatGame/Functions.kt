3ab04f55a45586f966636e51b9c2b70819f9d522
learngit <yasmymrdgs@gmail.com>
2022-01-10 10:42:25 +0000

    Added fun router()

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -4,4 +4,4 @@ package pt.ulusofona.lp2.deisiGreatGame
 enum class CommandType{GET, POST}
 //fun getPlayer(manager: GameManager, args: List<String>): String? { return ""}
 
-fun router(valor: List<String>): String { return ""}
+fun router(valor: Array<String>): String { return ""}
