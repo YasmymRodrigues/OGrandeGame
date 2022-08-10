99e7fa50c2d8913993f49a312c750ffcdde6f34e
learngit <yasmymrdgs@gmail.com>
2022-08-10 16:18:19 +0200

    working Functions.kt: postPlager()

--- src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
+++ src/pt/ulusofona/lp2/deisiGreatGame/Functions.kt
@@ -3,7 +3,18 @@ package pt.ulusofona.lp2.deisiGreatGame
 
 enum class CommandType{GET, POST}
 
+//Note: Router : obtem uma função que recebe um CommandType --> Função comando
+fun router(): (CommandType) -> (GameManager, List<String>) -> String?{return ::teste}
 
+fun teste(type: CommandType):(GameManager, List<String>) -> String?{
+    when(type){
+        CommandType.GET -> return::getPlayer
+        CommandType.POST -> return ::postPlayer
+    }
+}
+
+
+//DONE: GET COMMAND
 //Note: Função Comando(depende do comando que foi passado)
 fun getPlayer(manager: GameManager, args: List<String>): String? {
     var programmers = mutableListOf<Programmer>()
@@ -17,29 +28,20 @@ fun getPlayer(manager: GameManager, args: List<String>): String? {
         }
     }
     return "Inexistent player"
-
 }
 
-/*fun getPlayersByLanguage(manager: GameManager, ){
-
-}*/
-
-//Note: Router : obtem uma função que recebe um CommandType --> Função comando
-fun router(): (CommandType) -> (GameManager, List<String>) -> String?{return ::teste}
-
-
-fun teste(type: CommandType):(GameManager, List<String>) -> String?{return ::getPlayer}
-
+fun postPlayer(manager: GameManager, args: List<String>): String?{return null}
 
 val manager = GameManager()
 val routerFn = router()
+
+
+
 val commandGetFn = routerFn.invoke(CommandType.GET)
 
 
 //Note: Chamada da função comando
 val getPlayer = commandGetFn.invoke(manager, listOf("PLAYER", "Joshua"))
+val postPlayer = commandGetFn.invoke(manager, listOf("PLAYER", "Joshua"))
 
-
-
-//val getGuido = commandGetFn.invoke(manager, listOf("PLAYER", "Guido"))
 
